44a78bf2dd74166093bbdca2d86be0a87d9441ea
JudyPuff <judy.yun.ting@gmail.com>
2017-11-03 10:22:07 +1300

    add invisible function

--- game.js
+++ game.js
@@ -11,8 +11,9 @@ function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-    dots[i].addEventListener('contextmenu', makeBlue)
+    dots[i].addEventListener('contextmenu', makeGreen);
+    dots[i].addEventListener('contextmenu', makeBlue);
+    dots[i].addEventListener('contextmenu', hide);
     )
   }
 }
@@ -30,6 +31,10 @@ function makeBlue (evt) {
 }
 
 // CREATE FUNCTION hide HERE
+function hide(evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
