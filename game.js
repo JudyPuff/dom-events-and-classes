a4f8a740c0a5d2ba170db626c9c52571222487c9
JudyPuff <judy.yun.ting@gmail.com>
2017-11-04 14:07:39 +1300

    fix js syntax, dots change on click events

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls
 // the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+
+
+document.addEventListener('DOMContentLoaded', start);
 
 function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+  bindEventListeners(document.getElementsByClassName('board')[0].children);
 }
 
 function bindEventListeners (dots) {
@@ -14,26 +16,25 @@ function bindEventListeners (dots) {
     dots[i].addEventListener('contextmenu', makeGreen);
     dots[i].addEventListener('click', makeBlue);
     dots[i].addEventListener('dblclick', hide);
-    )
   }
 }
 
-function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+function makeGreen(evt) {
+  evt.preventDefault();
+  evt.target.classList.toggle('green');
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
-function makeBlue (evt) {
-  evt.target.classList.toggle('blue')
-  updateCounts()
+function makeBlue(evt) {
+  evt.target.classList.toggle('blue');
+  updateCounts();
 }
 
 // CREATE FUNCTION hide HERE
 function hide(evt) {
-  evt.target.classList.toggle('invisible')
-  updateCounts()
+  evt.target.classList.toggle('invisible');
+  updateCounts();
 }
 
 function updateCounts () {
@@ -41,20 +42,21 @@ function updateCounts () {
     blue: 0,
     green: 0,
     invisible: 0
-  }
+  };
+}
 
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-function displayTotals () { bindEventListeners(document.getElementsByClassName('totals')[0].children)
+function displayTotals () { bindEventListeners(document.getElementsByClassName('totals')[0].children);
   for (i = 0; i < total.length; i++) {
-    total[1].addEventListener('')
+    total[1].addEventListener('click');
   }
 
   // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+  displayTotals(totals);
 }
 
 function displayTotals (totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + '-total').innerHTML = totals[key];
   }
 }
